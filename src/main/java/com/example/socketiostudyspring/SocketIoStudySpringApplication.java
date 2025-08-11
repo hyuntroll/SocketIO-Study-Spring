0ebd379e695e4cd89be1d48dbaf3bb682bb06357
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-08-11 17:36:15 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/example/socketiostudyspring/SocketIoStudySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketiostudyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketIoStudySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketIoStudySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socketiostudyspring/SocketIoStudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketiostudyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketIoStudySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
