0c67cb8e2682d486ebdd73eab309ca11148f8779
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-08-16 01:28:41 +0900

    chore: 세션과 룸 카운트 메서드 및 room을 가져오는 코드 수정

--- src/main/java/com/example/socketiostudyspring/service/ChatRoomService.java
+++ src/main/java/com/example/socketiostudyspring/service/ChatRoomService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,19 +21,20 @@ public class ChatRoomService {
         return this.roomMap.containsKey(roomId);
     }
 
-    public Room getRoom(String roomId) {
+    private Room getRoom(String roomId) {
         return isRoom(roomId) ? this.roomMap.get(roomId) : null;
     }
 
     public boolean isCorrect(String roomId, String password) {
-        if (!isRoom(roomId)) { return false; }
+        if (!isRoom(roomId)) { return true; }
 
-        return getRoom(roomId).getPassword().equals(password);
+        return !getRoom(roomId).getPassword().equals(password);
     }
 
     public boolean createRoom(String roomId, String password) {
         if (isRoom(roomId)) { return false; }
 
+        log.info("room created: " + roomId);
         Room room = new Room(roomId, password);
         this.roomMap.put(roomId, room);
 
@@ -40,21 +42,50 @@ public class ChatRoomService {
     }
 
     public boolean deleteRoom(String roomId, String password) {
-        if (!isRoom(roomId) || !isCorrect(roomId, password)) { return false; }
+        if (!isRoom(roomId) || isCorrect(roomId, password)) { return false; }
 
+        log.info("room deleted: " + roomId);
         this.roomMap.remove(roomId);
         return true;
     }
 
-    public boolean joinRoom(String roomId, String password, String userId) {
-        if (!isRoom(roomId) || !isCorrect(roomId, password)) { return false; }
+    public int getRoomCount() {
+        return this.roomMap.size();
+    }
 
-        Room room = new Room(roomId, password);
+    public int getSessionCount(String roomId) {
+        if (!isRoom(roomId)) { return -1; }
+
+        return getRoom(roomId).getSessionCount();
+
+    }
+
+    public boolean joinRoom(String roomId, String password, String userId) {
+        if (!isRoom(roomId) || isCorrect(roomId, password)) { return false; }
+
+        Room room = getRoom(roomId);
         Set<String> roomSession = room.getSessionSet();
         if (roomSession.contains(userId)) { return false; }
 
         roomSession.add(userId);
         room.setSessionCount(room.getSessionCount()+1);
+        return true;
+    }
+
+    public boolean leaveRoom(String roomId, String password, String userId) {
+        if (!isRoom(roomId) || isCorrect(roomId, password)) { return false; }
+
+        Room room = getRoom(roomId);
+        Set<String> roomSession = room.getSessionSet();
+        if (roomSession.contains(userId)) { return false; }
+
+        roomSession.remove(userId);
+        room.setSessionCount(room.getSessionCount()-1);
+
+        if (room.getSessionCount() <= 0 ) {
+            deleteRoom(roomId, password);
+        }
+
         return true;
     }
 
